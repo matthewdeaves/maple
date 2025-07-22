de778a0554ddd0c4bd103def32c14e411df76e8d
Matthew Deaves <mndeaves@me.com>
2025-07-22 23:03:49 +0100

    user register page works and saves a user with validation

--- maple/users/views.py
+++ maple/users/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User
-
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def register_view(request):
-    return render(request, "users/user_register.html")
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("posts:list")
+    else:
+        form = UserCreationForm()
+    return render(request, "users/user_register.html", { "form":form })
