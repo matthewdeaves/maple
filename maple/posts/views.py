9a3037a262862ea002844c9d38e45999f653d130
Matthew Deaves <mndeaves@me.com>
2025-07-26 10:56:33 +0100

    added save new post functionality, migrations for user hasmany posts

--- maple/posts/views.py
+++ maple/posts/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-
+from .models import Post
+from . import forms
 
 # Create your views here.
 def posts_list(request):
@@ -14,4 +14,13 @@ def post_page(request, slug):
 
 @login_required(login_url="/users/login")
 def post_new(request):
-    return render(request, 'posts/post_new.html')
+    if request.method == 'POST':
+        form = forms.CreatePost(request.POST, request.FILES)
+        if form.is_valid():
+            newpost = form.save(commit=False)
+            newpost.author = request.user
+            newpost.save()
+            return redirect('posts:list')
+    else:
+        form = forms.CreatePost()
+    return render(request, 'posts/post_new.html', {'form': form})
